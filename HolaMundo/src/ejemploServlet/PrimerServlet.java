8fa3e7b00289eea65b3180643efa10bbad0c2c31
Zipe <Zipe@MSI>
2020-02-26 18:07:27 +0100

    Mejore de rendimiento

---
+++ HolaMundo/src/ejemploServlet/PrimerServlet.java
@@ -0,0 +1,51 @@
+package ejemploServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PrimerServlet
+ */
+@WebServlet("/PrimerServlet")
+public class PrimerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PrimerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out= response.getWriter();
+		response.setContentType("text/html");
+		
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD" + 
+		"HTML 4.0 Transititional//EN\">" +
+		"<html><head><title>Hola Mundo</title></head>" + "<body><h1> Hola Mundo"
+				+ "</h1></body></html>");
+
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
